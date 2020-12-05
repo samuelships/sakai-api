1e1e34c30878635d30b308140f0d2cf92b235aeb
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-12-05 10:46:31 +0000

    setup index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import axiosCookieJarSupport from "axios-cookiejar-support";
+import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from "axios";
+import { CookieJar } from "tough-cookie";
+
+export default class SakaiAPI {
+    
+}
