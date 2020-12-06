62e35c4df34f6a3dd3f909e29f332429582d856d
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-12-06 09:23:01 +0000

    add constructor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import axiosCookieJarSupport from "axios-cookiejar-support";
 import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from "axios";
+import API from "./types";
 import { CookieJar } from "tough-cookie";
 
 export default class SakaiAPI {
-    
+    config: API.SakaiAPIConfig;
+    cookieJar: CookieJar;
+    request: AxiosInstance;
+
+    constructor(config: API.SakaiAPIConfig = {}) {
+        this.config = {
+            baseUrl: config.baseUrl || "https://sakai.ug.edu.gh/",
+        } as API.SakaiAPIConfig;
+
+        this.cookieJar = new CookieJar();
+        this.request = axios.create({
+            baseURL: this.config.baseUrl,
+            jar: this.cookieJar,
+            withCredentials: true
+        } as AxiosRequestConfig);
+
+        axiosCookieJarSupport(this.request);
+    }
 }
---
+++ src/types/api.d.ts
@@ -0,0 +1,3 @@
+export interface SakaiAPIConfig {
+    baseUrl?: string,
+}
