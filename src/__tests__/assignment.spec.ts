9eabe78185410677bcdbf620d354f99457b43281
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2021-01-04 22:11:02 +0000

    test : add getSiteAssignment test

---
+++ src/__tests__/assignment.spec.ts
@@ -0,0 +1,18 @@
+import MockAdapter from "axios-mock-adapter";
+import SakaiApi from "../../src/index";
+import { loadTestData } from "./utils";
+
+
+describe("getSiteAssignment", () => {
+    it("respond with success", async () => {
+        const api = new SakaiApi();
+        const adapter = new MockAdapter(api.request);
+
+        adapter.
+            onGet(new RegExp("/direct/assignment/site/.*.json"))
+            .reply(200, loadTestData("getSiteAssignment.json"), {});
+
+        const response = await api.getSiteAssignment("xxx");
+        expect(response.status).toBe(200);
+    });
+});
