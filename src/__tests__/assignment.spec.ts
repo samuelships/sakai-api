768f393f3dce8deda0b6fc1f55ae1ed7fa50493c
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2021-01-06 14:01:15 +0000

    test : add getItemAssignment test

--- src/__tests__/assignment.spec.ts
+++ src/__tests__/assignment.spec.ts
@@ -16,3 +16,17 @@ describe("getSiteAssignment", () => {
         expect(response.status).toBe(200);
     });
 });
+
+describe("getItemAssignment", () => {
+    it("respond with success", async () => {
+        const api = new SakaiApi();
+        const adapter = new MockAdapter(api.request);
+
+        adapter.
+            onGet(new RegExp("/direct/assignment/item/.*.json"))
+            .reply(200, loadTestData("getItemAssignment.json"), {});
+
+        const response = await api.getItemAssignment("xxx");
+        expect(response.status).toBe(200);
+    });
+});
